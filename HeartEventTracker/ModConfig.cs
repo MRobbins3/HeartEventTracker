a6857798105568362c9cbb704743a65aa5fc4810
MRobbins3 <44211962+MRobbins3@users.noreply.github.com>
2021-05-26 23:10:53 -0500

    Add mod config with preliminary userful booleans, pull that config into the project via the entry method

---
+++ HeartEventTracker/ModConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartEventTracker
+{
+    class ModConfig
+    {
+        public bool ShowUnavailableEvents { get; set; }
+        public bool ShowAvailableEvents { get; set; }
+        public bool ShowSeenEvents { get; set; }
+        public bool ShowTriggerConditions { get; set; }
+        public bool ShowEventDescription { get; set; }
+
+
+
+        public ModConfig()
+        {
+            this.ShowUnavailableEvents = false;
+            this.ShowAvailableEvents = true;
+            this.ShowSeenEvents = true;
+            this.ShowTriggerConditions = true;
+            this.ShowEventDescription = false;
+        }
+    }
+}
--- HeartEventTracker/ModEntry.cs
+++ HeartEventTracker/ModEntry.cs
@@ -4,26 +4,25 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Menus;
 
 namespace HeartEventTracker
 {
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
-        /*********
-        ** Public methods
-        *********/
+
+        private ModConfig Config;
+
+
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+
         }
 
-
-        /*********
-        ** Private methods
-        *********/
         /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
@@ -34,7 +33,15 @@ namespace HeartEventTracker
                 return;
 
             // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+            // this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        }
+
+        /// <summary>Raised after the active menu is drawn but before it's actually rendered.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnRenderedActiveMenu(object sender, RenderedActiveMenuEventArgs e)
+        {
+
         }
     }
 }
